e7c101e64ba5d1ec53489ddf59503e8c5f33ffba
Mkremis <martinkremis@hotmail.com>
2023-08-26 12:37:00 -0300

    prisma

---
+++ src/app/new/page.jsx
@@ -0,0 +1,5 @@
+const NewPage = () => {
+  return <div>Formulario de tareas</div>;
+};
+
+export default NewPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>Tareas</div>;
 };
 
 export default HomePage;
