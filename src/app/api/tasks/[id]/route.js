47e3c5a0d800be5767309c5bfecd94c48dff7807
Mkremis <martinkremis@hotmail.com>
2023-08-26 14:11:08 -0300

    crud methods

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,57 @@
+import { prisma } from "@/libs/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  try {
+    const task = await prisma.task.findUnique({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    return NextResponse.json(task);
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+    });
+  }
+}
+
+export async function PUT(request, { params }) {
+  try {
+    const { title, description } = await request.json();
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: Number(params.id),
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    return NextResponse.json({
+      message: `Se ha actualizado la tarea: ${updatedTask.title}`,
+      data: updatedTask,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+    });
+  }
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const deletedTask = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+    return NextResponse.json({
+      message: `Se ha eliminado la tarea: ${deletedTask.title}`,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+    });
+  }
+}
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,31 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-  return NextResponse.json("Creando tareas");
+export async function GET() {
+  try {
+    const tasks = await prisma.task.findMany();
+    return NextResponse.json(tasks);
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+    });
+  }
+}
+
+export async function POST(request) {
+  try {
+    const { title, description } = await request.json();
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+      },
+    });
+
+    return NextResponse.json(newTask);
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+    });
+  }
 }
